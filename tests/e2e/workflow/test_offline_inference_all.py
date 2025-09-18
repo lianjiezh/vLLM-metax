7c67672ab8ae34507b10c9f3bd3e2ef89564320f
Hank <hcc.mayday@gmail.com>
2025-09-18 18:43:52 +0800

    enhancement: patch for env of e2e test
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- tests/e2e/workflow/test_offline_inference_all.py
+++ tests/e2e/workflow/test_offline_inference_all.py
@@ -2,7 +2,6 @@
 import pytest
 
 from tests.conftest import VllmRunner
-import os
 
 MLA_MODELS = ["deepseek-v2-lite"]
 NON_MLA_MODELS = ["qwen3-4b"]
@@ -15,14 +14,14 @@ NON_MLA_BACKENDS = ["FLASH_ATTN", "FLASHINFER"]
 @pytest.mark.parametrize("backend", MLA_BACKENDS)
 @pytest.mark.parametrize("enforce_eager", [True, False])
 @pytest.mark.parametrize("distributed_executor_backend", ["mp", "ray"])
-def test_mla_attn_backends(model: str, backend: str, enforce_eager: bool,
+def test_mla_attn_backends(monkeypatch, model: str, backend: str,
+                           enforce_eager: bool,
                            distributed_executor_backend: str) -> None:
     example_prompts = [
         "Hello, my name is",
         "The future of AI is",
     ]
-
-    os.environ["VLLM_ATTENTION_BACKEND"] = backend
+    monkeypatch.setenv("VLLM_ATTENTION_BACKEND", backend)
     with VllmRunner(model,
                     tensor_parallel_size=1,
                     max_model_len=4096,
@@ -30,20 +29,20 @@ def test_mla_attn_backends(model: str, backend: str, enforce_eager: bool,
                     distributed_executor_backend=distributed_executor_backend
                     ) as vllm_model:
         vllm_model.generate_greedy(example_prompts, 1024)
-    os.environ["VLLM_ATTENTION_BACKEND"] = ""
 
 
 @pytest.mark.parametrize("model", NON_MLA_MODELS)
 @pytest.mark.parametrize("backend", NON_MLA_BACKENDS)
 @pytest.mark.parametrize("enforce_eager", [True, False])
 @pytest.mark.parametrize("distributed_executor_backend", ["mp", "ray"])
-def test_non_mla_attn_backends(model: str, backend: str, enforce_eager: bool,
+def test_non_mla_attn_backends(monkeypatch, model: str, backend: str,
+                               enforce_eager: bool,
                                distributed_executor_backend: str) -> None:
     example_prompts = [
         "Hello, my name is",
         "The future of AI is",
     ]
-    os.environ["VLLM_ATTENTION_BACKEND"] = backend
+    monkeypatch.setenv("VLLM_ATTENTION_BACKEND", backend)
     with VllmRunner(model,
                     tensor_parallel_size=1,
                     max_model_len=4096,
@@ -51,4 +50,3 @@ def test_non_mla_attn_backends(model: str, backend: str, enforce_eager: bool,
                     distributed_executor_backend=distributed_executor_backend
                     ) as vllm_model:
         vllm_model.generate_greedy(example_prompts, 1024)
-    os.environ["VLLM_ATTENTION_BACKEND"] = ""
