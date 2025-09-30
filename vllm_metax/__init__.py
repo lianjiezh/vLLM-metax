759482169ec153c94ad5b570b060f0a5d9c3d5ab
Hank <hcc.mayday@gmail.com>
2025-09-30 16:03:54 +0800

    fix: typo in register
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/__init__.py
+++ vllm_metax/__init__.py
@@ -93,7 +93,6 @@ def collect_env() -> None:
 
 ########### platform plugin ###########
 def register():
-    register_patch()
     """Register the METAX platform."""
     return "vllm_metax.platform.MacaPlatform"
 
