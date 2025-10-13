d96c382c5832c1fec2df4bdfecc014ecee8b7b8e
Hank <hcc.mayday@gmail.com>
2025-10-13 17:54:25 +0800

    fix: sync MACA_VISIBLE_DEVICES with CUDA_VISIBLE_DEVICES
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/patch/__init__.py
+++ vllm_metax/patch/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # isort: skip_file
 from . import custom_op
+from . import maca_visible_device
 from . import distributed
 from . import device_allocator
 from . import model_executor
