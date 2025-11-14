30e2d25d400fa021a40a2a3f9db1fff03c237702
Hank <hcc.mayday@gmail.com>
2025-11-14 15:38:35 +0800

    dockerfile: support maca 3.2
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,4 +1,10 @@
 # Current directory should be the root of the repository
 
 # You may need to pass VLLM_VERSION to build a specific version
-docker build --network host -f docker/vllm_metax.Dockerfile -t vllm_metax:v0 --build-arg VLLM_VERSION=v0.11.0 .
+docker build \
+    --network host \
+    -f docker/vllm_metax.Dockerfile \
+    -t vllm_metax:v0 \
+    --build-arg VLLM_VERSION=v0.10.2 \
+    --build-arg MACA_VERSION=3.2.1 \
+     .
